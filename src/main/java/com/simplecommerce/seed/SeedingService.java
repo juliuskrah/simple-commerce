abd3cbb4c3ba3f64239ccf5100730bcb4eac083c
juliuskrah <9932477+juliuskrah@users.noreply.github.com>
2024-08-31 02:01:35 +0200

    feat(seed): stage media upload
    Add seed implementatioin for staged uploads and signed url

--- src/main/java/com/simplecommerce/seed/SeedingService.java
+++ src/main/java/com/simplecommerce/seed/SeedingService.java
@@ -4,22 +4,38 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.execution.ExecutionId;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.graphql.ExecutionGraphQlService;
 import org.springframework.graphql.support.DefaultExecutionGraphQlRequest;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Service
+@ConditionalOnProperty(prefix = "simple-commerce.seeder", name = "enabled", havingValue = "true")
 public class SeedingService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(SeedingService.class);
   private final ExecutionGraphQlService executionService;
   private final ResourceLoader resourceLoader;
   private final ObjectMapper objectMapper;
@@ -32,16 +48,7 @@ public class SeedingService {
     this.objectMapper = objectMapper;
   }
 
-  private void seedProductMedia(Set<Product> products) throws IOException {
-    var stageUpload = """
-        mutation stageUpload($input: StagedUploadInput!) {
-          stagedUpload(input: $input) {
-            presignedUrl
-            resourceUrl
-            contentType
-          }
-        }
-        """;
+  private void seedProductMedia(Flux<Product> products) throws IOException {
     var addProductToMedia = """
         mutation addMediaToProduct($id: ID!, $file: FileInput!) {
           addProductMedia(productId: $id, file: $file) {
@@ -54,20 +61,46 @@ public class SeedingService {
     var productMediaVariables = objectMapper.readValue(productMedia.getInputStream(),
         new TypeReference<Set<Map<String, Object>>>() {
         });
-    products.forEach(product ->
+    Set<Map<String, Object>> mediaVariables = products.map(product ->
       productMediaVariables.stream()
           .filter(media -> media.get("productId") == product.syntheticId())
-          .forEach(media -> media.put("id", product.naturalId()))
-    );
+          .peek(media -> media.put("id", product.naturalId()))
+          .collect(Collectors.toSet())
+    )
+        .blockLast();
     Flux.fromIterable(productMediaVariables)
         .flatMap(variable -> {
           @SuppressWarnings("unchecked")
-          var images = (List<Map<String, Object>>) variable.get("mediaLocations");
-          return Flux.fromIterable(images);
-        });
+          var images = (List<String>) variable.get("mediaLocations");
+          return Flux.fromIterable(images)
+              .flatMap(this::createStaged)
+              .publishOn(Schedulers.boundedElastic())
+              .map(staged -> {
+                var mediaPath = (String) staged.get("mediaPath");
+                var contentType = (String) staged.get("contentType");
+                var resource = resourceLoader.getResource(directoryPrefix + mediaPath);
+                try(var client = HttpClient.newHttpClient()) {
+                  var request = HttpRequest.newBuilder()
+                      .uri(URI.create((String) staged.get("presignedUrl")))
+                      .header("Content-Type", contentType)
+                      .PUT(BodyPublishers.ofFile(resource.getFile().toPath()))
+                      .build();
+                  var response = client.send(request, BodyHandlers.ofString());
+                  LOG.info("Upload status: {}", response.statusCode());
+                } catch (IOException | InterruptedException e) {
+                  LOG.info("Failed to upload media: {}", mediaPath, e);
+                }
+                return Map.of(
+                    "file", Map.of(
+                        "contentType", contentType,
+                        "resourceUrl", staged.get("resourceUrl")),
+                    "id", variable.get("id"));
+              });
+        }).log()
+        .blockLast();
   }
 
-  private Set<Product> seedProduct() throws IOException {
+  private Flux<Product> seedProduct() throws IOException {
     var makeProduct = """
         mutation makeProduct($input: ProductInput!) {
           addProduct(input: $input) {
@@ -83,28 +116,48 @@ public class SeedingService {
     return Flux.fromIterable(productVariables)
         .flatMap(variable -> {
           var product = new Product((String)variable.get("id"), null);
-          var requestVariables = removeId(variable);
+          removeId(variable, Set.of("id"));
           var result = executionService.execute(new DefaultExecutionGraphQlRequest(
-                makeProduct, null, requestVariables, null, id(), null));
+                makeProduct, null, Map.of("input", variable), null, id(), null));
           return result.map(response -> {
             String nodeId = response.field("addProduct.id").getValue();
             return product.withNaturalId(nodeId);
           });
         })
         .subscribeOn(Schedulers.boundedElastic())
-        .log()
-        .collect(Collectors.toSet())
-        .block();
+        .log();
   }
 
-  private Map<String, Object> removeId(Map<String, Object> products) {
-    var keysToRemove = Set.of("id");
+  private void removeId(Map<String, Object> products, Set<String> keysToRemove) {
     products.keySet().removeAll(keysToRemove);
-    return products;
   }
 
   private String id() {
     return ExecutionId.generate().toString();
+  }
+
+  private Mono<Map<String, Object>> createStaged(String filePath) {
+    var stageUpload = """
+        mutation stageUpload($input: StagedUploadInput!) {
+          stagedUpload(input: $input) {
+            presignedUrl
+            resourceUrl
+            contentType
+          }
+        }
+        """;
+    var fileName = Paths.get(filePath).getFileName().toString();
+    Map<String, Object> input = Map.of("input", Map.of("filename", fileName));
+    var stagedUpload = new DefaultExecutionGraphQlRequest(
+        stageUpload, null, input, null, id(), null);
+    return executionService.execute(stagedUpload).map(response -> {
+      response.field("addProduct.id").getValue();
+      String presignedUrl = response.field("stagedUpload.presignedUrl").getValue();
+      String resourceUrl = response.field("stagedUpload.resourceUrl").getValue();
+      String contentType = response.field("stagedUpload.contentType").getValue();
+      return Map.of("presignedUrl", presignedUrl, "resourceUrl", resourceUrl,
+          "contentType", contentType, "mediaPath", filePath);
+    });
   }
 
   private Resource loadResource(String path) {
@@ -112,8 +165,19 @@ public class SeedingService {
   }
 
   public void seed() throws IOException {
+    // TODO drop existing data
     var products = seedProduct();
-//    seedProductMedia(products);
+    seedProductMedia(products);
+  }
+
+  @Async
+  @EventListener
+  void on(ApplicationReadyEvent event) {
+    try {
+      seed();
+    } catch (IOException e) {
+      LOG.error("Failed to seed data", e);
+    }
   }
 
   record Product(String syntheticId, String naturalId) {
--- src/test/java/com/simplecommerce/seed/SeedingServiceTest.java
+++ src/test/java/com/simplecommerce/seed/SeedingServiceTest.java
@@ -2,6 +2,7 @@ package com.simplecommerce.seed;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
+import static org.mockito.AdditionalAnswers.answer;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -48,7 +49,7 @@ class SeedingServiceTest {
   @Mock
   private ResourceLoader resourceLoader;
   @Mock
-  ObjectMapper objectMapper;
+  private ObjectMapper objectMapper;
 
   @Test
   void shouldSeedData() throws IOException {
@@ -60,43 +61,84 @@ class SeedingServiceTest {
   }
 
   private void assertCaptured(List<DefaultExecutionGraphQlRequest> request) {
-    assertThat(request).isNotEmpty().hasSize(5)
+    assertThat(request).isNotEmpty().hasSize(15)
         .element(0, InstanceOfAssertFactories.type(DefaultExecutionGraphQlRequest.class))
         .extracting(ExecutionGraphQlRequest::getVariables, InstanceOfAssertFactories.MAP)
+        .extracting("input", InstanceOfAssertFactories.MAP)
         .containsOnlyKeys("description", "tags", "title")
         .containsExactly(
             entry("description", ""),
             entry("title", "Doom: Part One"),
             entry("tags", List.of("movie")));
+    // Assert stage product media
+    assertThat(request).element(5, InstanceOfAssertFactories.type(DefaultExecutionGraphQlRequest.class))
+        .extracting(ExecutionGraphQlRequest::getVariables, InstanceOfAssertFactories.MAP)
+        .extracting("input", InstanceOfAssertFactories.MAP)
+        .containsOnlyKeys("filename");
   }
 
   private void verification(ArgumentCaptor<DefaultExecutionGraphQlRequest> captor) throws IOException {
-    verify(executionService, times(5)).execute(captor.capture());
-    verify(objectMapper, times(1)).readValue(any(InputStream.class), ArgumentMatchers.<TypeReference<Set<Map<String, Object>>>>any());
-    verify(resourceLoader, times(1)).getResource(anyString());
+    // 1. Called 5 times insert products
+    // 2. Called 10 times stage product media
+    verify(executionService, times(15)).execute(captor.capture());
+    verify(objectMapper, times(2)).readValue(any(InputStream.class), ArgumentMatchers.<TypeReference<Set<Map<String, Object>>>>any());
+    verify(resourceLoader, times(1)).getResource(argThat(location -> location.contains("products.json")));
+    verify(resourceLoader, times(1)).getResource(argThat(location -> location.contains("product_media.json")));
+    verify(resourceLoader, times(5)).getResource(argThat(location -> location.contains("media/doom-part-one")));
+    verify(resourceLoader, times(5)).getResource(argThat(location -> location.contains("media/doom-part-two")));
   }
 
+  @SuppressWarnings("unchecked")
   private void stubs() throws IOException {
-    var resource = spy(new ClassPathResource(""));
-    when(executionService.execute(argThat(this::matchProduct))).thenReturn(createResponse());
+    var resource = spy(new ClassPathResource("seed-data/media/test-image.jpeg"));
+    when(executionService.execute(any(ExecutionGraphQlRequest.class))).thenAnswer(answer(this::answerRequest));
     when(resourceLoader.getResource(anyString())).thenReturn(resource);
     when(objectMapper.readValue(any(InputStream.class), ArgumentMatchers.<TypeReference<Set<Map<String, Object>>>>any()))
-        .thenReturn(products());
+        .thenReturn(products(), productMedia());
   }
 
-  private boolean matchProduct(ExecutionGraphQlRequest request) {
-    return request.getVariables().containsKey("tags")
-        && request.getVariables().containsKey("title")
-        && request.getVariables().containsKey("description");
+  @SuppressWarnings("unchecked")
+  private boolean matchProductRequest(ExecutionGraphQlRequest request) {
+    var input = (Map<String, String>) request.getVariables().get("input");
+    return input.containsKey("tags")
+        && input.containsKey("title")
+        && input.containsKey("description");
   }
 
-  private Mono<ExecutionGraphQlResponse> createResponse() {
+  @SuppressWarnings("unchecked")
+  private boolean matchStagedUploadRequest(ExecutionGraphQlRequest request) {
+    var input = (Map<String, String>) request.getVariables().get("input");
+    return input.containsKey("filename");
+  }
+
+  private Mono<ExecutionGraphQlResponse> createCreateProductResponse() {
     var input = ExecutionInput.newExecutionInput().query("mutation {}").build();
     var output = ExecutionResult.newExecutionResult().data(Map.of(
         "addProduct", Map.of(
             "id", "Z2lkOi8vU2ltcGxlQ29tbWVyY2UvUHJvZHVjdC9lNWVhODE5YS05NjQ1LTRiMTQtOWVjMy00NzgzZGY0Mzc0OWI="))).build();
     ExecutionGraphQlResponse response = new DefaultExecutionGraphQlResponse(input, output);
     return Mono.defer(() -> Mono.just(response));
+  }
+
+  private Mono<ExecutionGraphQlResponse> stagedUploadResponse() {
+    var input = ExecutionInput.newExecutionInput().query("mutation {}").build();
+    var output = ExecutionResult.newExecutionResult().data(Map.of(
+        "stagedUpload", Map.of(
+            "presignedUrl", "https://play.minio.io/bucket/prefix/object.jpg?signature=signature&expires=expires",
+            "resourceUrl", "https://play.minio.io/bucket/prefix/object.jpg",
+            "contentType", "image/jpeg")))
+        .build();
+    ExecutionGraphQlResponse response = new DefaultExecutionGraphQlResponse(input, output);
+    return Mono.defer(() -> Mono.just(response));
+  }
+
+  private Mono<ExecutionGraphQlResponse> answerRequest(ExecutionGraphQlRequest request) {
+    if (matchStagedUploadRequest(request)) {
+      return stagedUploadResponse();
+    } else if(matchProductRequest(request)) {
+      return createCreateProductResponse();
+    }
+    return null;
   }
 
   private Set<Map<String, Object>> products() {
@@ -127,5 +169,28 @@ class SeedingServiceTest {
         "description", "",
         "tags", List.of("movie"))));
     return products;
+  }
+
+  private Set<Map<String, Object>> productMedia() {
+    var productMedia = new HashSet<Map<String, Object>>();
+    productMedia.add(new HashMap<>(Map.of(
+        "productId", "4e8343d7-8dbe-408f-bd82-3732d6c3793d",
+        "mediaLocations", List.of(
+            "media/doom-part-one-1.jpg",
+            "media/doom-part-one-2.jpg",
+            "media/doom-part-one-3.jpg",
+            "media/doom-part-one-4.jpg",
+            "media/doom-part-one-5.jpg")
+    )));
+    productMedia.add(new HashMap<>(Map.of(
+        "productId", "512af1f2-acc0-4464-a6fb-71236b12ff43",
+        "mediaLocations", List.of(
+            "media/doom-part-two-1.jpg",
+            "media/doom-part-two-2.jpg",
+            "media/doom-part-two-3.jpg",
+            "media/doom-part-two-4.jpg",
+            "media/doom-part-two-5.jpg")
+    )));
+    return productMedia;
   }
 }
